21a464514a02b0497261406add436e3f7a1a7688
yarascript-dot <yarascript@gmail.com>
2025-12-30 14:55:26 +0200

    Create config.js

---
+++ habit-tracker/config.js
@@ -0,0 +1,5 @@
+const NOTION_TOKEN = "ntn_I90760609095CGkTYYdIHbvfTDfOkX03ErTzl3wZh6dgKZ";
+const DATABASE_ID = "Habit-Tracker-2c021cf9b2ed81faaac0cd24bd7deaa0";
+
+const PERCENT_PROPERTY = "Widgets"; 
+// назва колонки формули з відсотком
